a5b3fecba200f97dc5178089670213b62a1b7327
John Michael Robey <robey14@gmail.com>
2020-03-13 16:10:22 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   #fill that hash with key/value pairs that describe the following information: 
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 shipping_manifest = {}
-shipping_manifest={"whale bone corsets"=>5,"porcelain vases" => 2, "oil paintings" => 3}
+shipping_manifest={"whale bone corsets" => 5,"porcelain vases" => 2, "oil paintings" => 3}
 
 
 end
@@ -23,6 +23,9 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  shipping_manifest["pearl necklace"]=1
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   shipping_manifest
+end
+ 
+ 
